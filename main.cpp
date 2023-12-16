ec4d3ab288082595705b3f2674f72a3289bcbd73
CANITEY <mohamedmed01345@gmail.com>
2023-12-16 16:45:32 +0200

    emptied main

--- main.cpp
+++ main.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
 #include "doctors.h"
+#include "patients.h"
 
 using namespace std ;
 
 
 int main() {
-    Doctors doc;
-    doc.insertOrdered("mohammed");
-    doc.insertOrdered("ahmed");
-    doc.insertOrdered("abmed");
-    doc.insertOrdered("saeed");
-    doc.insertOrdered("sad");
-
-    doc.display();
-    cout << doc.length() << endl;
     return 0;
+
 }
